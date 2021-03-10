4062842daf906cf18e23722bca8904bb92f6ed5a
rihard363 <stragking6@gmail.com>
2021-03-10 17:30:28 +0300

    init: start and end task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n < 0 ){
+       n =  n * (-1);
+    };
+    let res = String(n);
+    let reversRes = res.split('').reverse().join('');
+    let result = Number(reversRes);
+    return result;
+};
